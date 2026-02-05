62c10009d355eb415fa477f68cef7e814085d359
Angel Carmichael <amcarmichael2@my.waketech,edu>
2026-02-04 22:19:27 -0500

    im sleepy

---
+++ Exercise2.java
@@ -0,0 +1,19 @@
+// Write your imports here if needed
+
+import java.util.*;
+
+public class Exercise2 {
+
+    // Write your code here
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        // line of integers read as a string
+        System.out.println("Enter integers for priority queue 1: ");
+        String expression = keyboard.nextLine();
+
+        // Split the string into part of an expression
+        String[] splitExpression = expression.split(" ");
+        // ughhhhhh
+    }
+}
